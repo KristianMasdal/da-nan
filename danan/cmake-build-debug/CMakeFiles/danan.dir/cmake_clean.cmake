0dd9844af37f759ede9c86601a70211abe1b82b9
Kristian Masdal <kristian.mdal@gmail.com>
2019-01-07 12:29:29 +0100

    Add files via upload

---
+++ danan/cmake-build-debug/CMakeFiles/danan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/danan.dir/main.c.o"
+  "danan.pdb"
+  "danan"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/danan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
